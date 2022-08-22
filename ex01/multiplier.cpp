d1c33d722cc48bebce9fa44acef5782363f4ad3e
pde-bakk <pde-bakk@student.codam.nl>
2022-08-22 14:38:53 +0200

    I prefer my first adder version

--- ex01/multiplier.cpp
+++ ex01/multiplier.cpp
@@ -4,12 +4,13 @@
 typedef unsigned int u32;
 
 static u32 adder(u32 a, u32 b) {
-	while (b) {
-		u32 carry = a & b;
-		a ^= b;
-		b = carry << 1;
+	while (a & b) {
+		u32 xor_res = a xor b;
+		u32 and_res = (a bitand b) << 1;
+		a = xor_res;
+		b = and_res;
 	}
-	return (a);
+	return (a | b);
 }
 
 auto multiplier(u32 a, u32 b) -> u32 {
